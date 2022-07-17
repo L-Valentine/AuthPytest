b1b38abec6f4839d8191929ab8a99b667773b7d7
khondakova_nadezhda <leja28@mail.ru>
2022-07-17 18:54:27 +0300

    Simple pytest case

---
+++ test_auth_2_fixture.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import pytest
+
+
+@pytest.fixture()
+def test_setup():
+    global driver
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.maximize_window()
+    yield
+    driver.close()
+    driver.quit()
+    print("Test completed")
+
+
+def test_login(test_setup):
+    driver.get(url='https://www.saucedemo.com/')
+    time.sleep(2)
+    user_name_field = driver.find_element(By.NAME, 'user-name')
+    user_name_field.send_keys('standard_user')
+    time.sleep(2)
+    user_password_field = driver.find_element(By.NAME, 'password')
+    user_password_field.send_keys('secret_sauce')
+    time.sleep(2)
+    login_button = driver.find_element(By.NAME, 'login-button')
+    login_button.click()
+    time.sleep(5)
